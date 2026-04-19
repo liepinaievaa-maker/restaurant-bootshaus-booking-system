7152da450fafae5d9b45825c243de7512d856fde
liepinaievaa-maker <liepinaievaa@gmail.com>
2026-04-19 19:41:14 +0300

    Add login logout and improve shared template structure

--- bootshaus/urls.py
+++ bootshaus/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from bookings.views import home, create_booking
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('book/', create_booking, name='create_booking'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
