993250e802febf56ee54bde2679e44c8c3f0ecc2
liepinaievaa-maker <liepinaievaa@gmail.com>
2026-04-20 14:37:07 +0300

    Style a booking page

--- bookings/forms.py
+++ bookings/forms.py
@@ -19,6 +19,22 @@ class BookingForm(forms.ModelForm):
         ]
 
         widgets = {
-            'booking_date': forms.DateInput(attrs={'type': 'date'}),
-            'booking_time': forms.TimeInput(attrs={'type': 'time'}),
+            'full_name': forms.TextInput(
+                attrs={'class': 'form-control'}
+            ),
+            'email': forms.EmailInput(
+                attrs={'class': 'form-control'}
+            ),
+            'booking_date': forms.DateInput(
+                attrs={'class': 'form-control', 'type': 'date'}
+            ),
+            'booking_time': forms.TimeInput(
+                attrs={'class': 'form-control', 'type': 'time'}
+            ),
+            'guests': forms.NumberInput(
+                attrs={'class': 'form-control'}
+            ),
+            'special_request': forms.Textarea(
+                attrs={'class': 'form-control'}
+            ),
         }
